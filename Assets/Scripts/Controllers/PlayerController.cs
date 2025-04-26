dd5354733f5487dfe00bfd5c75ac6b01fbcd7f26
zhizhikkkk <115149354+Abeke11@users.noreply.github.com>
2025-04-27 01:36:09 +0600

    add levels , add animations

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -1,22 +1,27 @@
-
 using UnityEngine;
 using UniRx;
 using Zenject;
 
 [RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(Animator))]
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float walkSpeed = 5f;
-    [SerializeField] float sprintSpeed = 8f;
-    [SerializeField] float jumpForce = 5f;
-    [SerializeField] Transform cameraHolder; 
+    [Header("Speeds")]
+    [SerializeField] private float walkSpeed = 5f;
+    [SerializeField] private float sprintSpeed = 8f;
+    [SerializeField] private float jumpForce = 5f;
 
-    IInputService _input;
-    CharacterController _cc;
-    Vector2 _moveInput;
-    Vector3 _velocity;
-    bool _isGrounded;
-    bool _isSprinting;
+    [Header("References")]
+    [SerializeField] private Transform cameraHolder;
+
+    private IInputService _input;
+    private CharacterController _cc;
+    private Animator _animator;
+
+    private Vector2 _moveInput;
+    private Vector3 _velocity;
+    private bool _isGrounded;
+    private bool _isSprinting;
 
     [Inject]
     public void Construct(IInputService inputService)
@@ -24,26 +29,28 @@ public class PlayerController : MonoBehaviour
         _input = inputService;
     }
 
-    void Awake()
+    private void Awake()
     {
         _cc = GetComponent<CharacterController>();
+        _animator = GetComponent<Animator>();
     }
 
-    void Start()
+    private void Start()
     {
         _input.MoveStream
               .Subscribe(v => _moveInput = v)
               .AddTo(this);
 
         _input.LookStream
-              .Subscribe(delta => {
+              .Subscribe(delta =>
+              {
                   transform.Rotate(Vector3.up, delta.x * Time.deltaTime * 100f);
                   cameraHolder.Rotate(Vector3.right, -delta.y * Time.deltaTime * 100f);
               })
               .AddTo(this);
-
         _input.JumpStream
-              .Subscribe(_ => {
+              .Subscribe(_ =>
+              {
                   if (_isGrounded)
                       _velocity.y = Mathf.Sqrt(jumpForce * -2f * Physics.gravity.y);
               })
@@ -57,17 +64,24 @@ public class PlayerController : MonoBehaviour
               .AddTo(this);
     }
 
-    void Update()
+    private void Update()
     {
         _isGrounded = _cc.isGrounded;
         if (_isGrounded && _velocity.y < 0)
             _velocity.y = -2f;
 
-        float speed = _isSprinting ? sprintSpeed : walkSpeed;
+        float targetSpeed = _isSprinting ? sprintSpeed : walkSpeed;
+
         Vector3 moveDir = transform.right * _moveInput.x + transform.forward * _moveInput.y;
-        _cc.Move(moveDir * speed * Time.deltaTime);
+
+        _cc.Move(moveDir * targetSpeed * Time.deltaTime);
 
         _velocity += Physics.gravity * Time.deltaTime;
         _cc.Move(_velocity * Time.deltaTime);
+
+        float currentSpeed = moveDir.magnitude * targetSpeed;
+        float normalized = Mathf.Clamp01(currentSpeed / sprintSpeed);
+
+        _animator.SetFloat("Speed", normalized);
     }
 }
